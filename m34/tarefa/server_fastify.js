7b8451f3c4ae54c87a817a17f27d5bd8b56bc42f
erickm31 <151151922+erickm31@users.noreply.github.com>
2026-03-31 19:39:39 -0300

    .

--- m34/tarefa/server_fastify.js
+++ m34/tarefa/server_fastify.js
@@ -1,35 +1,33 @@
 
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 
 const server = Fastify({ logger: false })
 
-// Rota principal
+
+
 server.get('/', async (request, reply) => {
   reply.send('Bem-vindo à página inicial!')
 })
 
-// Rota /sobre
 server.get('/sobre', async (request, reply) => {
   reply.send('Esta é a página Sobre.')
 })
 
-// Rota /contato
 server.get('/contato', async (request, reply) => {
   reply.send('Página de contato.')
 })
 
-// Rota para qualquer outro caminho (404)
 server.setNotFoundHandler(async (request, reply) => {
   reply.code(404).send('Página não encontrada.')
 })
 
-// Inicia o servidor na porta 3002 usando async/await
-const PORT = 3002
+const PORT = 3000
 try {
   await server.listen({ port: PORT })
   console.log(`Servidor rodando com FASTIFY  na porta ${PORT}`)
 } catch (err) {
-  // Se ocorrer erro ao iniciar o servidor, mostra o erro no log e encerra o processo
+
   server.log.error(err)
   process.exit(1)
 }
